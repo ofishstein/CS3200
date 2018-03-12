0aa23b30f29d568ca1a7155b8956ed6ca7c33e0b
joeyw526 <joeywood526@gmail.com>
2018-03-11 20:05:01 -0700

    Updated DML to match DDL
    Ensured at least 10 entries per relation

--- milestone/inserts.sql
+++ milestone/inserts.sql
@@ -1,17 +1,33 @@
 INSERT INTO Professional (firstName, lastName, picture, dob) 
-VALUES("Joseph", "Wood", "pic.jpg", "1995-05-26");
+VALUES("Joseph", "Wood", "joseph.jpg", "1995-05-26");
 
 INSERT INTO Professional (firstName, lastName, picture, dob) 
-VALUES("John", "Smith", "insta.jpg", "1995-12-25");
+VALUES("John", "Smith", "john.jpg", "1995-12-25");
 
 INSERT INTO Professional (firstName, lastName, picture, dob) 
-VALUES("Sara", "Jones", "fb.jpg", "1998-08-12");
+VALUES("Sara", "Jones", "sara.jpg", "1998-08-12");
 
 INSERT INTO Professional (firstName, lastName, picture, dob) 
-VALUES("Ann", "Williams", "twitter.jpg", "2010-02-28");
+VALUES("Ann", "Williams", "ann.jpg", "2010-02-28");
 
 INSERT INTO Professional (firstName, lastName, picture, dob) 
-VALUES("Kayla", "Davis", "google.jpg", "1952-04-01");
+VALUES("Kayla", "Davis", "kayla.jpg", "1952-04-01");
+
+INSERT INTO Professional (firstName, lastName, picture, dob) 
+VALUES("James", "Mccord", "james.jpg", "1901-06-10");
+
+INSERT INTO Professional (firstName, lastName, picture, dob) 
+VALUES("Cara", "Rosenblum", "cara.jpg", "2004-03-05");
+
+INSERT INTO Professional (firstName, lastName, picture, dob) 
+VALUES("Maxwell", "Kuzma", "maxwell.jpg", "2003-09-22");
+
+INSERT INTO Professional (firstName, lastName, picture, dob) 
+VALUES("Brigid", "Greaney", "brigid.jpg", "2000-06-30");
+
+INSERT INTO Professional (firstName, lastName, picture, dob) 
+VALUES("Duane", "Swift", "duane.jpg", "1997-12-18");
+
 
 INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
 VALUES("The Incredibles", "Action", "2004-11-05", "Pixar", "incredibles.jpg");
@@ -28,6 +44,21 @@ VALUES("Deadpool", "Action", "2016-02-12", "Fox", "deadpool.jpg");
 INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
 VALUES("The Dark Knight", "Action", "2008-07-18", "Warner Bros.", "the_dark_knight.jpg");
 
+INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
+VALUES("Titanic", "Drama", "1997-12-19", "Fox", "titanic.jpg");
+
+INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
+VALUES("Interstellar", "Drama", "2014-10-26", "Paramount", "interstellar.jpg");
+
+INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
+VALUES("Despicable Me", "Comedy", "2010-07-09", "Universal", "despicable_me.jpg");
+
+INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
+VALUES("Jaws", "Thriller", "1975-06-20", "Universal", "jaws.jpg");
+
+INSERT INTO Movie (name, genre, releaseDate, studio, coverPicture)
+VALUES("Casablanca", "Romance", "1943-01-23", "Warner Bros.", "casablanca.jpg");
+
 INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
 VALUES ("Jeff", "Johnson", "112 Huntington Ave", "Boston", "02120", "USA", "jeff@gmail.com", "p@$$w0rd");
 
@@ -43,8 +74,20 @@ VALUES ("Kelly", "Davis", "25256 Vista Sorrento Parkway", "San Diego", "92130", 
 INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
 VALUES ("Mariel", "Wilson", "450 Orland Park Road", "Chicago", "12670", "USA", "windycity@gmail.com", "blackhawks");
 
-INSERT INTO Vendor (name)
-VALUES ("Regal");
+INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
+VALUES ("Jim", "Gormley", "1000 Rodeo Drive", "Beverly Hills", "90212", "USA", "jim@gmail.com", "654321");
+
+INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
+VALUES ("Emily", "Madaras", "361A Old Finch Ave", "Toronto", "M1B 5K7", "Canada", "emily@northeastern.edu", "applefangirl");
+
+INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
+VALUES ("Zac", "Who", "1-5-2 Higashi-Shimbashi", "Tokyo", "105-7123", "Japan", "zac@intuit.com", "gfszdfeaffasd");
+
+INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
+VALUES ("Julie", "Frankel", "Rua do Farol 2", "Azoia", "2740-029", "Portugal", "julie@gmail.com", "fido0210");
+
+INSERT INTO SiteUser (firstName, lastName, streetAddr, city, postalCode, country, email, password)
+VALUES ("Savannah", "Lawrence", "15 Royal Mint St.", "London", "E1 8LG", "UK", "savannah@gmail.com", "football");
 
 INSERT INTO Vendor (name)
 VALUES ("Netflix");
@@ -53,10 +96,59 @@ INSERT INTO Vendor (name)
 VALUES ("Hulu");
 
 INSERT INTO Vendor (name)
+VALUES ("ABC");
+
+INSERT INTO Vendor (name)
+VALUES ("Bravo");
+
+INSERT INTO Vendor (name)
+VALUES ("FX");
+
+INSERT INTO Vendor (name)
+VALUES ("CBS");
+
+INSERT INTO Vendor (name)
+VALUES ("TBS");
+
+INSERT INTO Vendor (name)
+VALUES ("Disney");
+
+INSERT INTO Vendor (name)
+VALUES ("E");
+
+INSERT INTO Vendor (name)
+VALUES ("ESPN");
+
+INSERT INTO Vendor (name)
+VALUES ("Regal");
+
+INSERT INTO Vendor (name)
 VALUES ("AMC");
 
 INSERT INTO Vendor (name)
-VALUES ("ABC");
+VALUES ("Cinemark");
+
+INSERT INTO Vendor (name)
+VALUES ("EPIC");
+
+INSERT INTO Vendor (name)
+VALUES ("ArcLight");
+
+INSERT INTO Vendor (name)
+VALUES ("Regency");
+
+INSERT INTO Vendor (name)
+VALUES ("Reel");
+
+INSERT INTO Vendor (name)
+VALUES ("Tristone");
+
+INSERT INTO Vendor (name)
+VALUES ("MJR");
+
+INSERT INTO Vendor (name)
+VALUES ("Marquee");
+
 
 INSERT INTO Credit (role, personId, movieId)
 VALUES("Actor", 1, 5);
@@ -73,7 +165,20 @@ VALUES("Director", 4, 3);
 INSERT INTO Credit (role, personId, movieId)
 VALUES("Stunt Double", 5, 1);
 
+INSERT INTO Credit (role, personId, movieId)
+VALUES("Animation Director", 6, 8);
 
+INSERT INTO Credit (role, personId, movieId)
+VALUES("Film Editor", 7, 10);
+
+INSERT INTO Credit (role, personId, movieId)
+VALUES("Compositor", 8, 9);
+
+INSERT INTO Credit (role, personId, movieId)
+VALUES("Matte Painter", 9, 6);
+
+INSERT INTO Credit (role, personId, movieId)
+VALUES("Recording Engineer", 10, 7);
 
 
 INSERT INTO CreditCharacter (creditId, picture, name)
@@ -91,6 +196,20 @@ VALUES ("4", "deadpool.jpg", "Deadpool");
 INSERT INTO CreditCharacter (creditId, picture, name)
 VALUES ("5", "batman.jpg", "Batman");
 
+INSERT INTO CreditCharacter (creditId, picture, name)
+VALUES ("6", "jack.jpg", "Jack Dawson");
+
+INSERT INTO CreditCharacter (creditId, picture, name)
+VALUES ("7", "murph.jpg", "Murph");
+
+INSERT INTO CreditCharacter (creditId, picture, name)
+VALUES ("8", "gru.jpg", "Gru");
+
+INSERT INTO CreditCharacter (creditId, picture, name)
+VALUES ("9", "matt.jpg", "Matt Hopper");
+
+INSERT INTO CreditCharacter (creditId, picture, name)
+VALUES ("10", "rick.jpg", "Rick Blaine");
 
 
 INSERT INTO LovedMovies (movieId, userId)
@@ -108,121 +227,282 @@ VALUES (4, 1);
 INSERT INTO LovedMovies (movieId, userId)
 VALUES (5, 2);
 
+INSERT INTO LovedMovies (movieId, userId)
+VALUES (6, 6);
+
+INSERT INTO LovedMovies (movieId, userId)
+VALUES (7, 8);
+
+INSERT INTO LovedMovies (movieId, userId)
+VALUES (8, 9);
+
+INSERT INTO LovedMovies (movieId, userId)
+VALUES (9, 10);
+
+INSERT INTO LovedMovies (movieId, userId)
+VALUES (10, 7);
+
 
 
 
 INSERT INTO StreamingVendor(vendorId, url)
-VALUES (2, "www.netflix.com");
+VALUES (1, "www.netflix.com");
 
 INSERT INTO StreamingVendor(vendorId, url)
-VALUES (3, "www.hulu.com");
+VALUES (2, "www.hulu.com");
 
 INSERT INTO StreamingVendor(vendorId, url)
-VALUES (5, "www.abc.com");
+VALUES (3, "www.abc.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (4, "www.bravo.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (5, "www.fx.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (6, "www.cbs.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (7, "www.tbs.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (8, "www.disney.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (9, "www.e.com");
+
+INSERT INTO StreamingVendor(vendorId, url)
+VALUES (10, "www.espn.com");
 
 
 INSERT INTO TheaterVendor(vendorId, location)
-VALUES (1, "Fenway");
+VALUES (11, "Fenway");
 
 INSERT INTO TheaterVendor(vendorId, location)
-VALUES (4, "Dallas");
+VALUES (12, "San Francisco");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (13, "Dallas");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (14, "San Diego");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (15, "Los Angeles");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (16, "Chicago");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (17, "Houston");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (18, "Orlando");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (19, "Charleston");
+
+INSERT INTO TheaterVendor(vendorId, location)
+VALUES (20, "New York");
 
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2018-02-28 13:15:32", 1, 2, 4, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2018-02-28 13:15:32", 0.0, 1, 9, 1);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2003-02-23 12:25:26", 1, 2, 4, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2003-02-23 12:25:26", 0.0, 2, 1, 4);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2017-09-13 02:00:48", 4, 3, 5, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2017-09-13 02:00:48", 5.0, 3, 10, 5);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2018-01-15 18:49:23", 1, 2, 4, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2016-12-25 15:36:52", 4.99, 4, 5, 4);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2018-01-01 00:00:01", 5, 1, 3, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2005-07-04 17:55:20", 5.99, 5, 3, 3);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2013-09-04 03:04:05", 1, 2, 4, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2013-09-04 03:04:05", 0.0, 6, 2, 6);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2014-08-30 20:37:17", 2, 5, 1, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2012-04-20 14:18:31", 0.0, 7, 7, 9);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2011-6-30 23:59:59", 1, 2, 4, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2011-06-30 23:59:59", 0.0, 8, 4, 5);
 
-INSERT INTO MovieOrder(orderTime, vendorId, userId, movieId, dollarAmount)
-VALUES ("2002-05-26 22:23:24", 3, 4, 2, 27);
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2002-05-26 22:23:24", 6.0, 9, 8, 7);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2008-01-17 21:14:28", 0.0, 10, 3, 2);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2002-05-26 22:23:24", 10.0, 11, 9, 2);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2011-08-02 19:19:19", 66.0, 12, 2, 1);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2007-03-20 14:27:21", 18.99, 13, 6, 9);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2002-09-21 11:57:56", 45.0, 14, 5, 8);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2001-08-09 20:32:49", 48.0, 15, 4, 7);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2012-10-10 07:26:28", 8.0, 16, 10, 6);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2018-03-08 09:41:29", 53.0, 17, 7, 8);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2018-02-26 11:32:42", 38.0, 18, 6, 10);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2015-11-21 03:05:07", 15.0, 19, 1, 10);
+
+INSERT INTO MovieOrder(orderTime, dollarAmount, vendorId, userId, movieId)
+VALUES ("2016-10-31 04:06:08", 40.0, 20, 8, 1);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (11, 1);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (12, 6);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (13, 2);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (14, 4);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (15, 4);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (16, 1);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (17, 5);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (18, 3);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (19, 1);
+
+INSERT INTO TheaterOrder(confirmationNumber, ticketCount)
+VALUES (20, 2);
 
 
-INSERT INTO StreamingOrder(orderId, expiration)
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
 VALUES (1, "2018-04-21 11:16:28");
 
-INSERT INTO StreamingOrder(orderId, expiration)
-VALUES (3, "2018-02-27 08:19:43");
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (2, "2018-02-27 08:19:43");
 
-INSERT INTO StreamingOrder(orderId, expiration)
-VALUES (5, "2018-10-31 05:21:25");
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (3, "2018-10-31 05:21:25");
 
-INSERT INTO StreamingOrder(orderId, expiration)
-VALUES (7, "2016-12-25 15:36:52");
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (4, "2018-01-15 18:49:23");
 
-INSERT INTO StreamingOrder(orderId, expiration)
-VALUES (9, "2005-07-04 17:55:20");
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (5, "2018-01-01 00:00:01");
+
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (6, "2017-07-01 12:27:21");
+
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (7, "2014-08-30 20:37:17");
+
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (8, "2015-02-13 02:48:12");
+
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (9, "2010-05-21 06:42:51");
+
+INSERT INTO StreamingOrder(confirmationNumber, expiration)
+VALUES (10, "2009-11-30 23:37:10");
 
 
-INSERT INTO TheaterOrder(orderId, ticketCount)
-VALUES (2, 1);
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (11, "B8");
 
-INSERT INTO TheaterOrder(orderId, ticketCount)
-VALUES (4, 6);
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C2");
 
-INSERT INTO TheaterOrder(orderId, ticketCount)
-VALUES (6, 2);
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C3");
 
-INSERT INTO TheaterOrder(orderId, ticketCount)
-VALUES (8, 4);
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C1");
 
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C4");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (2, "B8"); # might want something here about theater numner and seat
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C5");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C2");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (12, "C6");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C3");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (13, "G10");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C1");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (13, "G09");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C4");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (14, "E5");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C5");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (14, "E6");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (4, "C6");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (15, "B3");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (6, "G10");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (15, "B4");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (6, "G09");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (16, "L1");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (8, "E5");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (17, "A10");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (8, "E6");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (17, "A11");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (8, "B3");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (17, "A9");
 
-INSERT INTO TheaterOrderSeats(orderId, seatId)
-VALUES (8, "B4");
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (17, "A12");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (17, "A8");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (18, "H3");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (18, "H4");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (18, "H2");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (19, "K20");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (20, "D8");
+
+INSERT INTO TheaterOrderSeats(confirmationNumber, seatId)
+VALUES (20, "D7");
 
 
 INSERT INTO MoviePictures(movieId, picture)
@@ -247,7 +527,19 @@ INSERT INTO MoviePictures(movieId, picture)
 VALUES (5, "commissionergorddon.png");
 
 INSERT INTO MoviePictures(movieId, picture)
-VALUES (5, "batman.png");
+VALUES (6, "jack.png");
+
+INSERT INTO MoviePictures(movieId, picture)
+VALUES (7, "tars.png");
+
+INSERT INTO MoviePictures(movieId, picture)
+VALUES (8, "gru.png");
+
+INSERT INTO MoviePictures(movieId, picture)
+VALUES (9, "matt.png");
+
+INSERT INTO MoviePictures(movieId, picture)
+VALUES (10, "rick.png");
 
 
 
